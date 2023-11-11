28f2195dfba68dcd411fe557076e7ea1a1294ed2
StapanulLumii <82084902+StapanulLumii@users.noreply.github.com>
2023-11-11 09:27:56 +0100

    Added sound for zombie when hes hurt

--- Assets/scripts/HealthZombie.cs
+++ Assets/scripts/HealthZombie.cs
@@ -7,6 +7,8 @@ public class HealthZombie : MonoBehaviour
     public float maxHealth;
     public float currentHealth;
     Ragdoll ragdoll;
+    public AudioSource audioSource;
+    public AudioClip ZombieHurt;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,7 @@ public class HealthZombie : MonoBehaviour
         currentHealth = maxHealth;
     }
     public void TakeDamage(int amount){
+        audioSource.PlayOneShot(ZombieHurt);
         currentHealth -= amount;
         if(currentHealth <= 0)
         {
