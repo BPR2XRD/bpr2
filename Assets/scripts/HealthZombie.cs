0c63a1b14e5f174e39cd470ba9402eea40d830d2
StapanulLumii <82084902+StapanulLumii@users.noreply.github.com>
2023-11-11 11:29:07 +0100

    Added respawn for the zombie player

--- Assets/scripts/HealthZombie.cs
+++ Assets/scripts/HealthZombie.cs
@@ -1,41 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthZombie : MonoBehaviour
 {
     public float maxHealth;
     public float currentHealth;
+    public GameObject zombiePrefab; // Assign the zombie prefab in the Inspector
+    public Transform spawnPoint; // Assign a spawn point in the Inspector
     Ragdoll ragdoll;
     public AudioSource audioSource;
     public AudioClip ZombieHurt;
+
     // Start is called before the first frame update
     void Start()
     {
         ragdoll = GetComponent<Ragdoll>();
         currentHealth = maxHealth;
     }
-    public void TakeDamage(int amount){
+
+    public void TakeDamage(int amount)
+    {
         audioSource.PlayOneShot(ZombieHurt);
         currentHealth -= amount;
-        if(currentHealth <= 0)
+        if (currentHealth <= 0)
         {
             Die();
-
         }
     }
-    private void Die(){
-        if(transform.CompareTag("Zombie"))
+
+    private void Die()
+    {
+        if (transform.CompareTag("Zombie"))
         {
             GetComponent<ZombieBehavior>().enabled = false;
         }
         ragdoll.ActivateRagdoll();
-        Destroy(gameObject, 10f);
+        InstantiateNewZombie();
+        Destroy(gameObject, 10f); // Adjust the time as needed before destroying the zombie
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InstantiateNewZombie()
     {
-        
+        if (zombiePrefab != null && spawnPoint != null)
+        {
+            Instantiate(zombiePrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+        else
+        {
+            Debug.LogError("Zombie prefab or spawn point not set!");
+        }
     }
 }
