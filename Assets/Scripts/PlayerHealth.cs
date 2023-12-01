ce10cf7218e919ce10a47b4685424c9fa538270f
StapanulLumii <82084902+StapanulLumii@users.noreply.github.com>
2023-12-01 12:34:00 +0100

    Made the Winning and losing screens

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,20 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Microlight.MicroBar;
+using System;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public GameObject canvas;
-     EndSceen endSceen;
+    public GameObject canvas1;
+    public GameObject canvas2;
+    public GameObject canvas3;
+    public GameObject Locomotion;
+    private EndSceen endScreen1;
+    private EndSceen endScreen2;
+    private EndSceen endScreen3;
     public int maxHealth = 100;
     public int currentHealth;
     public bool isDead = false;
     private MicroBar healthBar;
     private AudioSource audioSource;
 
+    public Transform TopViewCamera;
+    public Transform PlayersCamera;
+
 
     void Start()
     {
+        Time.timeScale = 1;
+        endScreen1 = canvas1.GetComponent<EndSceen>();
+        endScreen2 = canvas2.GetComponent<EndSceen>();
+        // endScreen3 = canvas3.GetComponent<EndSceen>();
+        
+
         audioSource = GetComponent<AudioSource>();
         currentHealth = maxHealth;
         isDead = false;
@@ -58,9 +73,21 @@ public class PlayerHealth : MonoBehaviour
 
         if (currentHealth <= 0 && !isDead)
         {
-            canvas.SetActive(true);
-            endSceen.Lose();
+            Debug.Log("canvas: " + canvas1);
+            Debug.Log("endScreen: " + endScreen1);
+            Debug.Log("Locomotion: " + Locomotion);
+            Debug.Log("PlayersCamera: " + PlayersCamera);
+            Debug.Log("TopViewCamera: " + TopViewCamera);
+
+            canvas1.SetActive(true);
+            canvas2.SetActive(true);
+            endScreen1.Lose();
+            endScreen2.Lose();
+            Locomotion.SetActive(false);
+
             Die();
+
+            Time.timeScale = 0;
         }
     }
 
--- Assets/scripts/EndSceen.cs
+++ Assets/scripts/EndSceen.cs
@@ -9,6 +9,13 @@ public class EndSceen : MonoBehaviour
 {
     
     public TextMeshProUGUI text;
+    public Sprite WinSprite;
+    public Sprite LoseSprite;
+
+    public Image image;
+    public AudioSource Source;
+    public AudioClip LoosingClip;
+    public AudioClip WinningClip;
 
 
     // Start is called before the first frame update
@@ -17,11 +24,19 @@ public class EndSceen : MonoBehaviour
         
     }
     public void Win(){
+        Source.Stop();
+        Source.PlayOneShot(WinningClip);
+
         text.text = "Wow, Ghost Hunter won. Congratulations!!!";
+        image.GetComponent<Image> ().sprite = WinSprite;
     }
 
     public void Lose(){
+        Source.Stop();
+        Source.PlayOneShot(LoosingClip);
+
         text.text = "O_O, Ghost Hunter was defeated";
+        image.GetComponent<Image> ().sprite = LoseSprite;
     }
     // Update is called once per frame
     void Update()
