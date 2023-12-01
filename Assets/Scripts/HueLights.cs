bc2d3a5ed8a942b8dfe8f8605970f8b03e5f34ee
Valeriu1 <74401589+Valeriu1@users.noreply.github.com>
2023-12-01 10:33:52 +0100

    Fix build errors

--- Assets/Scripts/HueLights.cs
+++ Assets/Scripts/HueLights.cs
@@ -6,7 +6,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using Light = Q42.HueApi.Light;
 using Q42.HueApi.ColorConverters.Original;
